68cdb7f6c2f125342f07364816350cbf17492062
Milosz Ogaza <miloszogaza@op.pl>
2017-11-01 17:25:23 +0100

    First clue solved, next clue at iteration: 139, num: 82683

--- puzzle_4.py
+++ puzzle_4.py
@@ -20,6 +20,14 @@ while counter <= 400:
     r = requests.get(url)
     soup = BeautifulSoup(r.content,'html.parser')
     text = str(soup.findAll(text=True))
-    num = (find('[0-9]{1,7}',text))
+
+    #according to the clue from iteration 84 I am supposed to divide
+    #'number to put in the next url' by two to get properly to iteration 85
+    #all other numbers should be passed as they are
+    if counter == 84:
+         num = str(int((find('[0-9]{1,7}',text)))/2)
+    else:
+        num = (find('[0-9]{1,7}',text))
+
     print("Iteration {}: next num to put in the url is: {} ".format(counter,num))
     counter += 1
