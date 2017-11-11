ed0787dcedba7d4b9d48712e72e063c86edb42d0
Milosz Ogaza <miloszogaza@op.pl>
2017-11-11 14:35:43 +0100

    The find function is implemented, it extracts a num from a text

--- test_p6.py
+++ test_p6.py
@@ -1,14 +1,29 @@
 #!usr/bin/env python
+#This program operates on the data inside the folder data_p6
 
-#initial number to start a chain
+import re
+
+# utility function operating on regular expressions suitable for the task
+def find(pat, text):
+    match = re.search(pat, text)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Function 'find' didn't find num in text")
+
+
+# initial number to start a chain
 num = '90052'
 
-#relative path to the file, witch should be opened next
+# relative path to the file, witch should be opened next
 rel_path = 'data_p6/{}.txt'.format(num)
 
+# opens a file from relative path and reads the content
+with open(rel_path, 'r') as f:
+    text = f.read()
 
-#TODO: using relative file path open a file and display to the console the text inside of it
+# extract num from a text using the find utility function
+num = (find('Next nothing is ([0-9]{1,7})', text))
 
-with open(rel_path,'r') as file:
-    text = file.read()
-    print(text)
+# prints the num on the screen
+print('Next num to put into a filename is: {}'.format(num))
